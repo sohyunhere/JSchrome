8c324d89596d2c1a85babb6daf8f7de9d24f1aa4
sohyunhere <70987069+sohyunhere@users.noreply.github.com>
2021-07-16 16:24:11 +0900

    fix background

--- background.js
+++ background.js
@@ -8,6 +8,6 @@ const images = ["0.jpg",
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `./img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
